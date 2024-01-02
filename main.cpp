e013a15de1a888c474944c4543be166abc09ac37
Fabien Proriol <fabien.proriol@kazoe.org>
2024-01-02 10:34:51 +0100

    Simple example for sftp send file

--- main.cpp
+++ main.cpp
@@ -1,11 +1,13 @@
 #include <QGuiApplication>
 #include <QQmlApplicationEngine>
-
+#include <sshclient.h>
+#include <sshsftpcommandsend.h>
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
 
+    /*
     QQmlApplicationEngine engine;
     const QUrl url(u"qrc:/QtSshExample/Main.qml"_qs);
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated,
@@ -14,6 +16,20 @@ int main(int argc, char *argv[])
                 QCoreApplication::exit(-1);
         }, Qt::QueuedConnection);
     engine.load(url);
+    */
 
-    return app.exec();
+    SshClient client;
+
+    client.setPassphrase("*****");
+    client.connectToHost("*****", "yourhost.org");
+    client.waitForState(SshClient::Ready);
+
+    qDebug() << "Connected";
+
+    SshSFtp *sftp = client.getChannel<SshSFtp>("sftp");
+    sftp->send("/etc/os-release", "/tmp/os-release");
+    qDebug() << "Copy OK";
+
+    //return app.exec();
+    return 0;
 }
